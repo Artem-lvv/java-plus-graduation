62f18673f8c185304b31a7785b09db382156f59e
Artem Shidlov <i@alvv.ru>
2024-12-30 16:06:10 +0300

    spring cloud ref

--- core/main-service/src/main/java/ru/yandex/practicum/category/ExploreWithMeApp.java
+++ core/main-service/src/main/java/ru/yandex/practicum/category/ExploreWithMeApp.java
@@ -2,8 +2,10 @@ package ru.yandex.practicum.category;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class ExploreWithMeApp {
     public static void main(String[] args) {
         SpringApplication.run(ExploreWithMeApp.class);
