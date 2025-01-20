abd04936483f7e3502fc09fb7c44bb38d29b286e
Artem Shidlov <i@alvv.ru>
2025-01-20 12:18:48 +0300

    rename catalog

--- recommendation/aggregator/src/main/java/ru/yandex/practicum/api/outData/kafka/KafkaTemplateOutComponent.java
+++ recommendation/aggregator/src/main/java/ru/yandex/practicum/api/outData/kafka/KafkaTemplateOutComponent.java
@@ -1,18 +1,18 @@
-package ru.yandex.practicum.api.out.kafka;
+package ru.yandex.practicum.api.outData.kafka;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.avro.specific.SpecificRecordBase;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Component;
+import ru.practicum.ewm.stats.avro.EventSimilarityAvro;
 
 @Slf4j
 @Component
 @RequiredArgsConstructor
 public class KafkaTemplateOutComponent {
-    private final KafkaTemplate<String, SpecificRecordBase> kafkaTemplate;
+    private final KafkaTemplate<String, EventSimilarityAvro> kafkaTemplate;
 
-    public void sendMessageKafka(String topic, SpecificRecordBase message) {
+    public void sendMessageKafka(String topic, EventSimilarityAvro message) {
         log.info("sendMessageKafka topic {} message {}", topic, message);
         kafkaTemplate.send(topic, message);
     }
